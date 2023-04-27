7cc1bbbd7e62741a0a8bfeb657b56f9663b2511b
angelsoempty <denatamanov1@gmail.com>
2023-04-27 20:52:08 +0300

    labwork ex_5

--- labwork.py
+++ labwork.py
@@ -1,11 +1,13 @@
 #4
-humans = [('Sasha', 16), ('Sanya', 17), ('Oleksandr', 18)]
-
 def fun(*args):
-    name_age = {}
-    if args:
-        for tup in args[0]:
-            name, age = tup
-            name_age[name] = age
-    return name_age
-print(fun(humans))
+    result_dict = {}
+    for arg in args:
+        result_dict.update(arg)
+    return result_dict
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+dict3 = {'c': 5, 'd': 6}
+
+result_dict = fun(dict1, dict2, dict3)
+print(result_dict)
