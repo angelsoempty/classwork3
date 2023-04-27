ac85ea865e95a6c1d79eea51156c2f9c9d923f85
angelsoempty <denatamanov1@gmail.com>
2023-04-27 18:47:58 +0300

    teoriya

--- 1.py
+++ 1.py
@@ -6,3 +6,10 @@ def black_hole(*args):
         print(argument)
 
 black_hole(234, 'Earth', 'rusnya', 'time', 1243)
+
+def black_hole_named(**kwargs):
+        print(type(kwargs))
+        print(kwargs)
+        for argument in kwargs:
+            print(argument)
+black_hole_named(name='Gleb', planet='Earth', number = 5)
