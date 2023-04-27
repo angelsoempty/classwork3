4bd362c6e093d9a629a8275c3335aafabc08a7ba
angelsoempty <denatamanov1@gmail.com>
2023-04-27 20:48:07 +0300

    labwork ex_4

--- labwork.py
+++ labwork.py
@@ -1,5 +1,11 @@
-#3
-def fun(*args):
-    print(' '.join((args)))
+#4
+humans = [('Sasha', 16), ('Sanya', 17), ('Oleksandr', 18)]
 
-fun('asd','asdasd','123123')
+def fun(*args):
+    name_age = {}
+    if args:
+        for tup in args[0]:
+            name, age = tup
+            name_age[name] = age
+    return name_age
+print(fun(humans))
