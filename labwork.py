7ba990cce492cba87cb1adcbebe5effc08f66568
angelsoempty <denatamanov1@gmail.com>
2023-05-02 16:52:04 +0300

    #5

--- labwork.py
+++ labwork.py
@@ -1,14 +1,13 @@
-#4
-def fun(*args):
-    result_dict = {}
-    for arg in args:
-        result_dict.update(arg)
-    return result_dict
+#5
+def func(*dicts):
+    result = {}
+    for d in dicts:
+        result.update(d)
+    return result
 
 dict1 = {'a': 1, 'b': 2}
-dict2 = {'b': 3, 'c': 4}
-dict3 = {'c': 5, 'd': 6}
+dict2 = {'c': 3, 'd': 4}
+dict3 = {'b': 5, 'e': 6}
 
-result_dict = fun(dict1, dict2, dict3)
-print(result_dict)
-''
+result = func(dict1, dict2, dict3)
+print(result)
